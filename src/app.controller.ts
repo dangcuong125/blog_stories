e9fc6090d8d411aae4c8b2cad4e8718fdcff8d76
Cuong Dang <dangcuong1225@gmail.com>
2023-12-07 11:44:51 +0700

    init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor() {}
 
   @Get()
-  // @AuthenticateCustomer()
   getHello() {
     return 'trigger build 1';
   }
--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   );
 
   app.enableCors({ origin: '*', credentials: true });
-  // app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
     .setTitle('Project API')
@@ -23,12 +23,12 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/swagger', app, document, {
+  SwaggerModule.setup('/api/swagger', app, document, {
     swaggerOptions: {
       persistAuthorization: true,
     },
   });
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(+process.env.PORT || 3000);
 }
 bootstrap();
