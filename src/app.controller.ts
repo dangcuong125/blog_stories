362878297c89bde3822c9ff19a9bca88ba3482a5
Cuong Dang <dangcuong1225@gmail.com>
2023-12-18 17:11:55 +0700

    fix api get bill

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
+import { User } from './auth/entities/user.entity';
+import {
+  AuthenticateUser,
+  CurrentUser,
+} from './common/decorators/auth.decorator';
 
 @Controller()
+@AuthenticateUser()
 export class AppController {
   constructor() {}
 
   @Get()
-  getHello() {
-    return 'trigger build 1';
+  getHello(@CurrentUser() user: User) {
+    return user;
   }
 }
